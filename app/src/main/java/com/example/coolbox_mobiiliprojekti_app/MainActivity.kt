e5370412067d12ea6a32a69e48b18aca75ebcc1b
Miika Haapanen <miika.haapanen@edu.lapinamk.fi>
2024-03-29 02:34:02 +0200

    Fixattu drawerlayout paddings ja lisätty myös home (mainscreen) drawerlayouttiin. Lisätty iconeita ja disabloitu sivustaveto login ja register screeneissä.

--- app/src/main/java/com/example/coolbox_mobiiliprojekti_app/MainActivity.kt
+++ app/src/main/java/com/example/coolbox_mobiiliprojekti_app/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.coolbox_mobiiliprojekti_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ColorLens
@@ -13,6 +15,7 @@ import androidx.compose.material.icons.filled.DashboardCustomize
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LockOpen
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
@@ -24,8 +27,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -39,9 +44,11 @@ import com.example.coolbox_mobiiliprojekti_app.view.ProductionScreen
 import com.example.coolbox_mobiiliprojekti_app.view.ThemesScreen
 import kotlinx.coroutines.launch
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             CoolBoxmobiiliprojektiAppTheme {
                 Surface(
@@ -61,7 +68,35 @@ class MainActivity : ComponentActivity() {
                         drawerState = drawerState,
                         drawerContent = {
                             ModalDrawerSheet {
-                                Spacer(modifier = Modifier.height(20.dp))
+
+                                // Drawerlayoutin logo column
+
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 20.dp)
+                                    ,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Top
+                                ) {
+                                    Text(
+                                        fontSize = 20.sp,
+                                        text = "Meidän hieno logo tms"
+                                    )
+                                    Row {
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Home"
+                                        )
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Home"
+                                        )
+                                    }
+                                    HorizontalDivider(modifier = Modifier.padding(top = 10.dp))
+                                }
+
+                                // Drawerlayoutin itemit
 
                                 NavigationDrawerItem(
                                     modifier = Modifier
@@ -145,6 +180,7 @@ class MainActivity : ComponentActivity() {
                                         )
                                     }
                                 )
+
                             }
                         }
                     ) {
@@ -216,10 +252,12 @@ class MainActivity : ComponentActivity() {
                                     }
                                 )
                             }
+
                         }
                     }
                 }
             }
         }
     }
+
 }
