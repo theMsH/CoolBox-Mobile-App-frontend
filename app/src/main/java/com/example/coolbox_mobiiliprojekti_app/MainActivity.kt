3934daf7c67a60a840cc079c2a6183a689e6f5fa
Benjamin Lukin <benjamin.lukin@edu.lapinamk.fi>
2024-04-07 02:49:25 +0300

    loginScreen laitettu takaisin defaultti startdestinationiksi navigoinnissa koska unohdin laittaa sen takaisin edellisessä commitissa lol

--- app/src/main/java/com/example/coolbox_mobiiliprojekti_app/MainActivity.kt
+++ app/src/main/java/com/example/coolbox_mobiiliprojekti_app/MainActivity.kt
@@ -237,7 +237,7 @@ class MainActivity : ComponentActivity() {
                     ) {
                         NavHost(
                             navController = navController,
-                            startDestination = "mainScreen"
+                            startDestination = "loginScreen"
                         ) {
                             composable("loginScreen") {
                                 LoginScreen(
